b5c4f7409de9ec9bf7508554c927fa644cfc3285
george.nicolae <george.nicolae@orange.com>
2020-03-30 10:35:36 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-it
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { DropdownDirective } from './shared/dropdown.directive';
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { AppRoutingModule } from "./app-routing.module";
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -22,9 +25,11 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
-  imports: [FormsModule, BrowserModule],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../../recipe.model";
 import { RecipeService } from "../../recipe.service";
+import { InvokeFunctionExpr } from "@angular/compiler";
 
 @Component({
   selector: "app-recipe-item",
@@ -10,12 +11,10 @@ import { RecipeService } from "../../recipe.service";
 export class RecipeItemComponent implements OnInit {
   @Input()
   recipe: Recipe;
+  @Input()
+  index: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onRecipeSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
